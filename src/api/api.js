d31907b403d64b821fcd861730009dddab124c07
mousragheb <ragheb.moustafa@yahoo.com>
2025-04-21 20:45:42 -0500

    Fix API baseURL for deployed backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://rtm-backend-m2j6.onrender.com'
+  baseURL: 'https://rtm-backend-m2j6.onrender.com', // make sure this is correct!
 });
 
 export default API;
